755f2cc4443ca5b3199996b7f01b14fc737419f8
Jerry L Hoover <jerry.hoover@ymail.com>
2020-04-23 16:23:23 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let runic_title =
+  "&#x16EC" +
+  "&#x16B1;" +
+  "&#x16A8;" +
+  "&#x16BE;" +
+  "&#x16DE;" +
+  "&#x16DF;" +
+  "&#x16D7;" +
+  " " +
+  "&#x16EB" +
+  " " +
+  "&#x16B1;" +
+  "&#x16A2;" +
+  "&#x16BE;" +
+  "&#x16CA;" +
+  "&#x16EC";
+
+let title = document.getElementById("title");
+title.innerHTML = runic_title;
+
+let fehu = "&#x16A0;";
+let runic_result = document.getElementById("runic-result");
+runic_result.innerHTML = fehu;
